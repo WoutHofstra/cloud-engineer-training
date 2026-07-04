0ee09f15eab96207f8da42831af7adad104e7d8d
Wout Hofstra <hofstrawout@gmail.com>
2026-07-04 10:59:41 +0200

    changed static to Static

--- project1-Azure-Lab/main.tf
+++ project1-Azure-Lab/main.tf
@@ -108,7 +108,7 @@ resource "azurerm_public_ip" "main" {
   name = "pip-bastion-001"
   location = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  allocation_method = "static"
+  allocation_method = "Static"
   sku = "Standard"
 }
 
