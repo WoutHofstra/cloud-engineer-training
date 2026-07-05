c13edfc8970586130533466bca35440ef16bc52e
Wout Hofstra <hofstrawout@gmail.com>
2026-07-05 09:34:35 +0200

    change back to west europe

--- project1-Azure-Lab/main.tf
+++ project1-Azure-Lab/main.tf
@@ -15,12 +15,12 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "main" {
   name = "rg-azurelab-001"
-  location = "North Europe"
+  location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "main" {
   name = "vnet-azurelab-001"
-  location = "North Europe"
+  location = "West Europe"
   resource_group_name = azurerm_resource_group.main.name
   address_space = ["10.0.0.0/16"]
 }
