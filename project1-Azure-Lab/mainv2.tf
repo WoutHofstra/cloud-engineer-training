b51355c01d47e224103593ca5c90c89cefd802ea
Wout Hofstra <hofstrawout@gmail.com>
2026-07-06 15:14:47 +0200

    Written project1 version 2

--- project1-Azure-Lab/mainv2.tf
+++ project1-Azure-Lab/mainv2.tf
@@ -1,3 +1,6 @@
+variable "allowed_ip_rdp" {
+  
+}
 
 terraform {
   required_version = ">= 1.8.0"
@@ -33,18 +36,19 @@ resource "azurerm_virtual_network" "main" {
   address_space = ["10.0.0.0/16"]
 }
 
+resource "azurerm_public_ip" "main" {
+  name = "pip-vm-001"
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method = "Static"
+  sku = "Standard"
+}
+
 resource "azurerm_subnet" "workload" {
   name = "subnet-azurelab-vm"
   resource_group_name = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes = ["10.0.1.0/24"]
-}
-
-resource "azurerm_subnet" "bastion" {
-  name = "AzureBastionSubnet"
-  resource_group_name = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes = ["10.0.2.0/26"]
 }
 
 resource "azurerm_network_security_group" "workload" {
@@ -112,23 +116,5 @@ resource "azurerm_linux_virtual_machine" "main" {
   }
 }
 
-resource "azurerm_public_ip" "main" {
-  name = "pip-bastion-001"
-  location = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  allocation_method = "Static"
-  sku = "Standard"
-}
 
-resource "azurerm_bastion_host" "main" {
-  name = "bastion-azurelab-001"
-  location = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  sku = "Developer"
 
-  ip_configuration {
-    name = "config"
-    subnet_id = azurerm_subnet.bastion.id
-    public_ip_address_id = azurerm_public_ip.main.id
-  }
-}
